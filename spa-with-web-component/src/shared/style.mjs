abcf8317813c85f88c9658aafd279ae1b935edd5
yama-yama-yama <yamayama.sc.1514@gmail.com>
2025-04-26 10:42:43 +0900

    Update style.mjs

--- spa-with-web-component/src/shared/style.mjs
+++ spa-with-web-component/src/shared/style.mjs
@@ -1,7 +1,9 @@
 export const BASIC_STYLE = /* CSS */ `
+  font-family: 'Noto Sans JP', sans-serif ;
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Noto Sans JP', sans-serif ;
   }
 `;
